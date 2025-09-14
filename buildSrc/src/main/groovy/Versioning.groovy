202e418519785e30905b6b3ad7a6e7db19106cce
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-09-14 13:23:16 +0200

    Add doc comments to Versioning

--- buildSrc/src/main/groovy/Versioning.groovy
+++ buildSrc/src/main/groovy/Versioning.groovy
@@ -4,7 +4,18 @@ import org.eclipse.jgit.revwalk.RevCommit
 import org.eclipse.jgit.revwalk.RevWalk
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 
+/**
+ * Utility class for determining the version of the project based on Git tags.
+ */
 class Versioning {
+
+    /**
+     * Get the version of the project based on Git tags. If the current commit has a tag, return that tag.  If not,
+     * return the latest tag with the abbreviated commit hash. If no tags are found, return "0.0.0-<abbreviatedHash>".
+     *
+     * @param projectRootDir the root directory of the project (containing the .git directory)
+     * @return the version string
+     */
     static String getVersion(File projectRootDir) {
         try {
             def repository = new FileRepositoryBuilder()
@@ -16,13 +27,17 @@ class Versioning {
             def git = new Git(repository)
 
             def headCommit = new RevWalk(repository).parseCommit(repository.resolve("HEAD"))
-
             def tags = git.tagList().call()
+
+            // ----------
+            // if no tags found, return 0.0.0-<abbreviatedHash>
             if (tags.isEmpty()) {
                 def hash = headCommit.id.name().substring(0, 7)
                 return "0.0.0-${hash}"
             }
 
+            // ----------
+            // check if there's a tag on the current HEAD and return it if found
             def tagOnHead = tags.find {
                 Ref ref ->
                     def commitId = ref.getPeeledObjectId() ?: ref.getObjectId()
@@ -32,6 +47,8 @@ class Versioning {
                 return tagOnHead.getName().replaceAll('refs/tags/', '')
             }
 
+            // ----------
+            // find the latest tag by commit time and return <latestTag>-<abbreviatedHash>
             def latestTag = tags.collect {
                 Ref ref ->
                     def commitId = ref.getPeeledObjectId() ?: ref.getObjectId()
