8aeba2e974c1174a029d68664c6d6044070d02e7
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-09-21 11:23:43 +0200

    Configuration new components

--- src/main/java/io/github/malczuuu/problem4j/spring/web/ProblemAutoConfiguration.java
+++ src/main/java/io/github/malczuuu/problem4j/spring/web/ProblemAutoConfiguration.java
@@ -1,16 +1,20 @@
 package io.github.malczuuu.problem4j.spring.web;
 
 import io.github.malczuuu.problem4j.jackson.ProblemModule;
+import io.github.malczuuu.problem4j.spring.web.formatting.DefaultDetailFormatting;
+import io.github.malczuuu.problem4j.spring.web.formatting.DetailFormatting;
+import io.github.malczuuu.problem4j.spring.web.formatting.FieldNameFormatting;
+import io.github.malczuuu.problem4j.spring.web.formatting.JacksonFieldNameFormatting;
+import java.util.List;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jackson.JacksonProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties(ProblemProperties.class)
-@Import(ProblemResponseEntityExceptionHandler.class)
 public class ProblemAutoConfiguration {
 
   @ConditionalOnMissingBean(ProblemModule.class)
@@ -26,5 +30,27 @@ public class ProblemAutoConfiguration {
   @Bean
   public ExceptionLoggingAdapter exceptionLoggingAdapter() {
     return new ExceptionLoggingAdapter();
+  }
+
+  @ConditionalOnMissingBean(DetailFormatting.class)
+  @Bean
+  public DetailFormatting detailFormatting(ProblemProperties properties) {
+    return new DefaultDetailFormatting(properties.getDefaultDetailFormat());
+  }
+
+  @ConditionalOnMissingBean(FieldNameFormatting.class)
+  @Bean
+  public FieldNameFormatting fieldNameFormatting(JacksonProperties properties) {
+    return new JacksonFieldNameFormatting(properties.getPropertyNamingStrategy());
+  }
+
+  @ConditionalOnMissingBean(ProblemResponseEntityExceptionHandler.class)
+  @Bean
+  public ProblemResponseEntityExceptionHandler problemResponseEntityExceptionHandler(
+      DetailFormatting detailFormatting,
+      FieldNameFormatting fieldNameFormatting,
+      List<ExceptionAdapter> exceptionAdapters) {
+    return new ProblemResponseEntityExceptionHandler(
+        detailFormatting, fieldNameFormatting, exceptionAdapters);
   }
 }
